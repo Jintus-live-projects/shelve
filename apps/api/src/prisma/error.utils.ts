219b8f613b75d083412e678d6a14f30d48d13430
Justin GALLY <justin.gallysp@gmail.com>
2023-03-07 23:07:19 +0100

    fix(api): hide prisma error exception data from response
    Refs: #9

--- apps/api/src/prisma/error.utils.ts
+++ apps/api/src/prisma/error.utils.ts
@@ -2,10 +2,16 @@ import { HttpException, InternalServerErrorException } from '@nestjs/common';
 
 export const catchPrismaClientError = (
   error: { code?: string; meta?: any },
-  mapping: Record<string, (meta?: any) => HttpException>,
+  mapping?: Record<string, (meta?: any) => HttpException>,
+  internalErrorMessage?: string,
 ): Promise<never> => {
   if (error.code && mapping.hasOwnProperty(error.code)) {
     return Promise.reject(mapping[error.code](error.meta));
   }
-  return Promise.reject(new InternalServerErrorException(error));
+  console.error(error);
+  return Promise.reject(
+    new InternalServerErrorException(
+      internalErrorMessage ?? 'Unexpected error occurred',
+    ),
+  );
 };
